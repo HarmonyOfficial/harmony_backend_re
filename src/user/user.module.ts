67ff3487cfa33a86817948b7de3511ba0243cd1e
oyuseong <oys@c.com>
2023-12-05 09:18:23 +0900

    bug fix

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,12 +3,12 @@ import { UsersService } from './user.service';
 import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
-import { HttpService } from '@nestjs/axios';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
   providers: [UsersService],
   controllers: [UserController],
-  imports: [TypeOrmModule.forFeature([User]), HttpService],
+  imports: [TypeOrmModule.forFeature([User]), HttpModule],
   exports: [TypeOrmModule],
 })
 export class UserModule {}
