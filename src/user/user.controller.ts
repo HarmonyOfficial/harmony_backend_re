652bf5f0b44926a3539b21c3c46e561088cce354
oyuseong <oys@c.com>
2023-12-06 14:21:22 +0900

    Commit

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,10 +3,14 @@ import {
   Post,
   UseInterceptors,
   UploadedFile,
+  Body,
+  UseGuards,
+  Request,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { multerConfig } from './multer.config';
 import { UsersService } from './user.service';
+import { JwtStrategy } from '../auth/jwt-auth.guard';
 
 @Controller('user')
 export class UserController {
@@ -17,5 +21,25 @@ export class UserController {
   async uploadProfile(@UploadedFile() file: Express.Multer.File) {
     const imagePath = `uploads/profiles/${file.filename}`;
     return { path: imagePath };
+  }
+
+  @Post('register')
+  @UseGuards(JwtStrategy)
+  async registerUser(@Request() req, @Body() body) {
+    const uid = req.user.uid; // JWT 토큰에서 UID 추출
+    const { name, profile_picture } = body;
+
+    // 임시 저장된 사용자 정보를 가져오는 로직
+    const tempUserData = await this.usersService.getTemporaryUser(uid);
+
+    // 데이터베이스에 사용자 정보 저장
+    await this.usersService.createOrUpdateUser({
+      uid: uid,
+      name: name,
+      email: tempUserData.userData.email,
+      profileImage: profile_picture,
+    });
+
+    return { success: true };
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,12 +2,15 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
+import { TemporaryUser } from '../auth/temporary-user.entity';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User)
     private userRepository: Repository<User>,
+    @InjectRepository(TemporaryUser)
+    private temporaryUserRepository: Repository<TemporaryUser>, // 임시 사용자 리포지토리 추가
   ) {}
 
   async userExists(uid: string) {
@@ -15,5 +18,25 @@ export class UsersService {
       where: { uid },
     });
     return !!user;
+  }
+
+  async getTemporaryUser(uid: string): Promise<TemporaryUser> {
+    return this.temporaryUserRepository.findOne({ where: { uid } });
+  }
+
+  async createOrUpdateUser(userData: any): Promise<User> {
+    let user = await this.userRepository.findOne({
+      where: { uid: userData.uid },
+    });
+
+    if (!user) {
+      user = new User();
+      user.uid = userData.uid;
+    }
+    user.name = userData.name;
+    user.email = userData.email;
+    user.profileImage = userData.profileImage;
+
+    return this.userRepository.save(user);
   }
 }
